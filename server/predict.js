88aa274c3cc172b6196b6ef1610b8d118dd70c1a
MilkFansHelloWorld <31027215+MilkFansHelloWorld@users.noreply.github.com>
2021-01-01 21:29:31 +0000

    Changed model to CNN

--- server/predict.js
+++ server/predict.js
@@ -2,7 +2,7 @@ const tf = require('@tensorflow/tfjs-node');
 const { exception } = require('console');
 const fs = require('fs');
 
-const MODELPATH = __dirname + '/model/model.json';
+const MODELPATH = __dirname + '/model/cnn_model/model.json';
 
 let model = null;
 
@@ -17,15 +17,16 @@ const predictFromImage = async (imagePath, reverseColor=1) => {
         await loadModel();
     }
     const image = fs.readFileSync(imagePath);
-    let image_tensor = tf.node.decodePng(image);
+    let image_tensor = tf.node.decodePng(image, 1);
     image_tensor = image_tensor.resizeBilinear([28, 28]);
     if (reverseColor==1) {
         image_tensor = image_tensor.add(-255).mul(-1);
     }
-    image_tensor = image_tensor.mean(-1).mul(1.0/255.0).expandDims(0);
+    image_tensor = image_tensor.mean(-1).mul(1.0/255.0).expandDims(0).expandDims(-1);
     const predictions = model.predict(image_tensor).flatten();
     const p = await predictions.argMax().data();
-    return p[0];
+    const proba = await predictions.data();
+    return [p[0], proba[p[0]]];
 }
 
 
@@ -38,7 +39,7 @@ const predictFromArray = async (array) => {
     // console.log(image_tensor.shape)
     fs.writeFileSync('./test/test.png', await tf.node.encodePng(image_tensor.mul(255).expandDims(-1)));
     // Just for test
-    image_tensor = image_tensor.expandDims(0);
+    image_tensor = image_tensor.expandDims(0).expandDims(-1);
     const predictions = model.predict(image_tensor).flatten();
     predictions.print();
     const p = await predictions.argMax().data();
