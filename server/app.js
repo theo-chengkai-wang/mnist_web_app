5ed1b4e5e9df5ab7decaa7bdcd9c2439bd5a84f5
MilkFansHelloWorld <31027215+MilkFansHelloWorld@users.noreply.github.com>
2021-01-07 16:15:47 +0000

    Small changes, added model class to back end

--- server/app.js
+++ server/app.js
@@ -7,7 +7,8 @@ const upload = multer({
     dest: 'uploads/'
 });
 
-const predict = require('./predict');
+//const predict = require('./predict');
+const {constants, model} = require('./model');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
@@ -19,26 +20,37 @@ app.get('/', (req, res) => {
 });
 
 app.post('/image', upload.single('file'), async (req, res) => {
+    if (model.mode!==constants.TEST) {
+        model.setMode(constants.TEST);
+    }
     if (!req.file) {
         res.status(400).send("No file uploaded")
     }
     console.log("file name: " + req.file.filename);
     console.log("mimetype: " + req.file.mimetype);
     console.log("reverse color? " + req.body.rev)
-    const result = await predict.predictFromImage(`./uploads/${req.file.filename}`, req.body.rev);
+    const {prediction, probability} = await model.predictFromImage(`./uploads/${req.file.filename}`, req.body.rev);
     console.log("result: " + result);
-    res.json({
-        result: result
-    });
+    res.json({prediction, probability});
 });
 
 app.post('/array', async(req, res) => {
+    if (model.mode!==constants.TEST) {
+        model.setMode(constants.TEST);
+    }
     const array = req.body.array;
-    const results = await predict.predictFromArray(array);
-    res.json({
-        result: results[0],
-        proba: results[1]
-    });
+    const results = await model.predictFromArray(array);
+    res.json(results);
+})
+
+app.post('/arraytrain', async(req, res) => {
+    if (model.mode!==constants.TRAIN) {
+        model.setMode(constants.TRAIN);
+    }
+    const array = req.body.array;
+    const label = req.body.label;
+    const results = await model.predictFromArray(array, label);
+    res.json(results)
 })
 
 module.exports = app;
